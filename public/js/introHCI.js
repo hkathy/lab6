e5a4bd5c7332a55b6deeaf108efee07c46421e12
Kathy Huynh <huynh.k100@gmail.com>
2016-02-11 16:04:54 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -8,16 +8,16 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
-function initializePage() {
-	$('.project a').click(addProjectDetails);
+ function initializePage() {
+ 	$('.project a').click(addProjectDetails);
 
-	$('#colorBtn').click(randomizeColors);
-}
+ 	$('#colorBtn').click(randomizeColors);
+ }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
-function addProjectDetails(e) {
+ function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
 
@@ -26,13 +26,40 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	var projectURL = "/project/" + idNumber;
+
 	console.log("User clicked on project " + idNumber);
+
+	console.log("URL: " + projectURL);
+
+	function addProject(result) {
+		console.log(result);
+		var projectHTML = '<img src="' + result['image']+'" class="detailsImage">'+
+		'<h3>'+result['date']+'</h3>'
+		+result['summary'];
+		$("#project"+idNumber+" div.details").html(projectHTML);
+		console.log("#project"+idNumber+" div.details");
+	};
+
+	$(".project").click(function(e) {
+		$.get(projectURL, addProject);
+		console.log(projectURL);
+	});
 }
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
-function randomizeColors(e) {
-	console.log("User clicked on color button");
-}
+ function randomizeColors(e) {
+ 	console.log("User clicked on color button");
+ 	$.get("/palette", function(e){
+ 		var colors = e.colors.hex;
+
+ 		$('body').css('background-color', colors[0]);
+ 		$('.thumbnail').css('background-color', colors[1]);
+ 		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+ 		$('p').css('color', colors[3]);
+ 		$('.project img').css('opacity', .75);
+ 	})
+ }
